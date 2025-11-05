976087d2923e056521417c5d60f50c405528e31e
cbrookscode <colbybrooks@myyahoo.com>
2025-11-05 11:32:31 -0500

    added some basic handlers for site metrics and resetting counter

--- main.go
+++ main.go
@@ -1,17 +1,51 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (a *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		a.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (a *apiConfig) handlerMetrics(reswrit http.ResponseWriter, req *http.Request) {
+	reswrit.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	reswrit.WriteHeader(http.StatusOK)
+	new := fmt.Sprintf("Hits: %v\n", a.fileserverHits.Load())
+	reswrit.Write([]byte(new))
+}
+
+func (a *apiConfig) handlerReset(reswrit http.ResponseWriter, req *http.Request) {
+	reswrit.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	reswrit.WriteHeader(http.StatusOK)
+	a.fileserverHits.Swap(0)
+	new := fmt.Sprintf("Coutner has been reset: %v\n", a.fileserverHits.Load())
+	reswrit.Write([]byte(new))
+}
 
 func main() {
 	const filepathroot = "."
 	const port = "8080"
 
+	api := &apiConfig{}
+
 	srvmux := http.NewServeMux()
-	srvmux.Handle("/", http.FileServer(http.Dir(filepathroot)))
+	srvmux.Handle("/app/", api.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathroot)))))
+	srvmux.HandleFunc("/healthz", handlerReadiness)
+	srvmux.HandleFunc("/metrics", api.handlerMetrics)
+	srvmux.HandleFunc("/reset", api.handlerReset)
+
 	srv := http.Server{
 		Handler: srvmux,
 		Addr:    ":" + port,
@@ -29,4 +63,10 @@ func main() {
 
 	log.Printf("serving on port %v\n", port)
 	log.Fatal(srv.ListenAndServe())
+}
+
+func handlerReadiness(reswrit http.ResponseWriter, req *http.Request) {
+	reswrit.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	reswrit.WriteHeader(http.StatusOK)
+	reswrit.Write([]byte("OK"))
 }
