7da45df364f0a092ea0632b188ba24fad2e09b79
cbrookscode <colbybrooks@myyahoo.com>
2025-11-07 15:42:45 -0500

    abid

--- internal/auth/authentication.go
+++ internal/auth/authentication.go
@@ -1,9 +1,13 @@
 package auth
 
 import (
+	"fmt"
 	"runtime"
+	"time"
 
 	"github.com/alexedwards/argon2id"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 )
 
 func HashPassword(password string) (string, error) {
@@ -30,4 +34,41 @@ func CheckPasswordHash(password, hash string) (bool, error) {
 		return true, nil
 	}
 	return false, nil
+}
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	now := &jwt.NumericDate{Time: time.Now().UTC()}
+	later := &jwt.NumericDate{Time: time.Now().UTC().Add(expiresIn)}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+		Issuer:    "chirpy",
+		IssuedAt:  now,
+		ExpiresAt: later,
+		Subject:   userID.String(),
+	})
+	tokenString, err := token.SignedString([]byte(tokenSecret))
+	if err != nil {
+		return "", fmt.Errorf("couldn't sign string with provided secret: %v", err)
+	}
+	return tokenString, nil
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	parsedToken, err := jwt.ParseWithClaims(tokenString, &jwt.RegisteredClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(tokenSecret), nil
+	})
+	if err != nil {
+		return uuid.Nil, err
+	}
+	if parsedToken.Valid {
+		uuidString, err := parsedToken.Claims.GetSubject()
+		if err != nil {
+			return uuid.Nil, err
+		}
+		validUUID, err := uuid.Parse(uuidString)
+		if err != nil {
+			return uuid.Nil, err
+		}
+		return validUUID, nil
+	}
+	return uuid.Nil, fmt.Errorf("invalid token")
 }
---
+++ internal/auth/authentication_test.go
@@ -0,0 +1,42 @@
+package auth
+
+import (
+	"testing"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+func TestCreateAndValidateJWT(t *testing.T) {
+	type testToken struct {
+		id          uuid.UUID
+		tokenSecret string
+		duration    time.Duration
+	}
+
+	tests := []testToken{
+		{uuid.New(), "dinosaurs", time.Minute},
+		{uuid.New(), "computers", time.Minute},
+		{uuid.New(), "wiley", time.Minute},
+		{uuid.New(), "booger", time.Minute},
+		{uuid.New(), "puke", time.Minute},
+	}
+
+	for _, tt := range tests {
+		tt := tt // capture variable at current loop iteration
+		t.Run(tt.tokenSecret, func(t *testing.T) {
+			t.Parallel() // run each subtest concurrently
+			tokenString, err := MakeJWT(tt.id, tt.tokenSecret, tt.duration)
+			if err != nil {
+				t.Errorf("error making jwt: %v", err)
+			}
+			tokenUUID, err := ValidateJWT(tokenString, tt.tokenSecret)
+			if err != nil {
+				t.Errorf("error validating jwt: %v", err)
+			}
+			if tokenUUID != tt.id {
+				t.Errorf("ids did not match: got %v, want %v", tokenUUID, tt.id)
+			}
+		})
+	}
+}
